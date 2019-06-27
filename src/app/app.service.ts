4a0521b03ad500243c3d5d8953fba01dbe07ae2a
binjiechen <ydostyle@163.com>
2019-06-27 18:30:07 +0800

    使用history模式

--- src/app/app.service.ts
+++
@@ -1,9 +0,0 @@
-import { Injectable } from '@angular/core';
-import { ReplaySubject } from 'rxjs';
-
-@Injectable({
-  providedIn: 'root'
-})
-export class AppService {
-  language$ = new ReplaySubject<string>(1);
-}
